caff6db0ce97893f4f68d28ab1f7a56eb4245cb6
wensir <wenjinghuan999@126.com>
2017-04-20 17:08:45 +0800

    change static to unnamed namespace

--- SmokeSolver/AdvectMethod.cu
+++ SmokeSolver/AdvectMethod.cu
@@ -3,44 +3,47 @@
 #include "AdvectMethod.h"
 using namespace ssv;
 
-
-// Semi-Lagrangian Advection
-// LAUNCH : block (ny), thread (nx)
-// p : nx x ny
-// q : nx x ny
-// u : nx x ny
-template <typename QType>
-static __global__ void kernelAdvectMethodSemiLagrangian(
-	Blob<QType>::wrapper_t qout, cudaTextureObject_t q, Blob<T2>::wrapper_const_t u
-)
+namespace
 {
-	uint y = blockIdx.x;
-	uint x = threadIdx.x;
+	using ssv::uint;
 
-	T2 p0 = make_float2(x, y) + (T)(0.5) - u(x, y);
+	// Semi-Lagrangian Advection
+	// LAUNCH : block (ny), thread (nx)
+	// p : nx x ny
+	// q : nx x ny
+	// u : nx x ny
+	template <typename QType>
+	__global__ void kernelAdvectMethodSemiLagrangian(
+		BlobWrapper<QType> qout, cudaTextureObject_t q, BlobWrapperConst<T2> u
+	)
+	{
+		uint y = blockIdx.x;
+		uint x = threadIdx.x;
 
-	qout(x, y) = tex2D<QType>(q, p0.x, p0.y);
+		T2 p0 = make_float2(x, y) + (T)(0.5) - u(x, y);
+
+		qout(x, y) = tex2D<QType>(q, p0.x, p0.y);
+	}
+
+	// Semi-Lagrangian Advection
+	// LAUNCH : block (ny, nz), thread (nx)
+	// p : nx x ny x nz
+	// q : nx x ny x nz
+	// u : nx x ny x nz
+	template <typename QType>
+	__global__ void kernelAdvectMethodSemiLagrangian(
+		BlobWrapper<QType> qout, cudaTextureObject_t q, BlobWrapperConst<T4> u
+	)
+	{
+		uint z = blockIdx.y;
+		uint y = blockIdx.x;
+		uint x = threadIdx.x;
+
+		T4 p0 = make_float4(x, y, z, 0) + (T)(0.5) - u(x, y, z);
+
+		qout(x, y, z) = tex3D<QType>(q, p0.x, p0.y, p0.z);
+	}
 }
-
-// Semi-Lagrangian Advection
-// LAUNCH : block (ny, nz), thread (nx)
-// p : nx x ny x nz
-// q : nx x ny x nz
-// u : nx x ny x nz
-template <typename QType>
-static __global__ void kernelAdvectMethodSemiLagrangian(
-	Blob<QType>::wrapper_t qout, cudaTextureObject_t q, Blob<T4>::wrapper_const_t u
-)
-{
-	uint z = blockIdx.y;
-	uint y = blockIdx.x;
-	uint x = threadIdx.x;
-
-	T4 p0 = make_float4(x, y, z, 0) + (T)(0.5) - u(x, y, z);
-
-	qout(x, y, z) = tex3D<QType>(q, p0.x, p0.y, p0.z);
-}
-
 
 template <typename QType>
 void AdvectMethodSemiLagrangian<QType>::operator () (
